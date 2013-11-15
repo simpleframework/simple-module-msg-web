fb54aa494e76d26b358ad957f8da13642f7a1853
simpleframework <cknet@126.com>
2013-11-15 12:11:18 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/msg/web/page/t1/NoticeMessageContentPage.java
+++ src/main/java/net/simpleframework/module/msg/web/page/t1/NoticeMessageContentPage.java
@@ -36,8 +36,7 @@ import net.simpleframework.mvc.template.lets.LCTemplateWinPage;
 /**
  * Licensed under the Apache License, Version 2.0
  * 
- * @author 陈侃(cknet@126 .com, 13910090885)
- *         https://github.com/simpleframework
+ * @author 陈侃(cknet@126 .com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public class NoticeMessageContentPage extends LCTemplateWinPage implements IMessageContextAware {
