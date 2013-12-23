3ede10ab5b664efd73aacce605644f4209bcf0e2
simpleframework <cknet@126.com>
2013-12-23 19:03:18 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/msg/web/page/MailSentPage.java
+++ src/main/java/net/simpleframework/module/msg/web/page/MailSentPage.java
@@ -5,6 +5,7 @@ import net.simpleframework.mvc.common.element.InputElement;
 import net.simpleframework.mvc.common.element.RowField;
 import net.simpleframework.mvc.common.element.TableRow;
 import net.simpleframework.mvc.common.element.TableRows;
+import net.simpleframework.mvc.component.ui.autocomplete.AutocompleteBean;
 
 /**
  * Licensed under the Apache License, Version 2.0
@@ -15,9 +16,19 @@ import net.simpleframework.mvc.common.element.TableRows;
 public class MailSentPage extends AbstractSentMessagePage {
 
 	@Override
+	protected void onForward(final PageParameter pp) {
+		super.onForward(pp);
+
+		addComponentBean(pp, "MailSentPage_autocomplete", AutocompleteBean.class).setInputField(
+				"ms_senter");
+	}
+
+	@Override
 	protected TableRows getTableRows(final PageParameter pp) {
+		final InputElement ms_senter = new InputElement("ms_senter");
+
 		final InputElement ms_content = InputElement.textarea("ms_content").setRows(10);
-		final TableRow r1 = new TableRow(new RowField("发送人", new InputElement()));
+		final TableRow r1 = new TableRow(new RowField("发送人", ms_senter));
 		final TableRow r2 = new TableRow(new RowField("主题", new InputElement()));
 		final TableRow r3 = new TableRow(new RowField("内容", ms_content, sm_content_bar));
 		return TableRows.of(r1, r2, r3);
