bd2e027401e2ecae5ad301536d8b6fbf41ac316c
CHENK <CHENK@CHENK-PC>
2016-01-16 16:06:36 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/module/msg/web/IMessageWebContext.java
+++ src/main/java/net/simpleframework/module/msg/web/IMessageWebContext.java
@@ -3,7 +3,6 @@ package net.simpleframework.module.msg.web;
 import net.simpleframework.module.msg.IMessageContext;
 import net.simpleframework.module.msg.web.plugin.PrivateMessagePlugin;
 import net.simpleframework.module.msg.web.plugin.SystemMessagePlugin;
-import net.simpleframework.mvc.IMVCContextVar;
 import net.simpleframework.mvc.PageParameter;
 import net.simpleframework.mvc.common.element.AbstractElement;
 
@@ -13,7 +12,7 @@ import net.simpleframework.mvc.common.element.AbstractElement;
  * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
-public interface IMessageWebContext extends IMessageContext, IMVCContextVar {
+public interface IMessageWebContext extends IMessageContext {
 
 	MessageUrlsFactory getUrlsFactory();
 
